e2348ed34bbe2b27c72fd21078a7d913a79dab02
zhanhongchen <zhanhongchen@sian.com>
2013-12-27 11:14:29 +0800

    测试

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define ( 'APP_PATH', './Application/' );
 
 define ( 'APP_DEBUG', true );
+echo 'aaaaaaaaaaaaaa';
 
 if(!is_file(APP_PATH . 'User/Conf/config.php')){
 	header('Location: ./install.php');
